3c3ec68885fa2770089edebcf26b351a29f4b578
MBVelkova <monikabv98@gmail.com>
2017-01-15 20:12:50 +0200

    Update and rename Program.cs to Expression.cs

--- Expression.cs
+++ Expression.cs
@@ -1,14 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Expression
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.WriteLine((3522 + 52353) * 23 - (2336 * 501 + 23432 - 6743) * 3);
         }
