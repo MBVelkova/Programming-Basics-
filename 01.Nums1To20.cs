e484589d85534bf0d41e461ca4c55a86a4b6b94b
MBVelkova <monikabv98@gmail.com>
2017-01-15 20:18:07 +0200

    Update and rename Program.cs to 01.Nums1To20.cs

--- 01.Nums1To20.cs
+++ 01.Nums1To20.cs
@@ -1,14 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Nums1To20
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.WriteLine(1);
             Console.WriteLine(2);
