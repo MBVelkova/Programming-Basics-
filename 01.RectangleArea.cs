eb722912c3a253b515090487c356d08535efbd45
MBVelkova <monikabv98@gmail.com>
2017-01-15 20:22:12 +0200

    Create 01.RectangleArea.cs

---
+++ 01.RectangleArea.cs
@@ -0,0 +1,18 @@
+using System;
+
+
+namespace Rectangle_Area
+{
+    class Program
+    {
+        static void Main()
+        {
+            var a = decimal.Parse(Console.ReadLine());
+            var b = decimal.Parse(Console.ReadLine());
+            // TODO: calculate the area and print it
+            Console.WriteLine(a * b);
+        }
+
+
+    }
+}
