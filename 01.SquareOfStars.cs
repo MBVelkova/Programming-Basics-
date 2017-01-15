5af256f304133b36a5cd21d95cef87f2f59998c8
MBVelkova <monikabv98@gmail.com>
2017-01-15 20:23:13 +0200

    Create 01.SquareOfStars.cs

---
+++ 01.SquareOfStars.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace SquareOfStars
+{
+    class Program
+    {
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            Console.WriteLine(new string('*', n));
+            for (int i = 0; i < n - 2; i++)
+            {
+                Console.WriteLine('*' + new string(' ', n - 2) + '*');
+            }
+            Console.WriteLine(new string('*', n));
+
+        }
+    }
+}
